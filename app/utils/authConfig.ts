183f8c0600f503239710bf9effddd50bdc4c91a0
AmmarFahmy <ammarkhan375@gmail.com>
2025-02-28 18:46:01 +0530

    Add local development configuration for Azure AD authentication

--- app/utils/authConfig.ts
+++ app/utils/authConfig.ts
@@ -6,6 +6,13 @@ export const msalConfig: Configuration = {
         authority: 'https://login.microsoftonline.com/77e3b5a4-521b-4caf-a71c-1f4d9d0d549e',
         redirectUri: 'https://ms-graph-next-app.vercel.app'
     }
+
+    // when running locally, comment out the above and uncomment the below
+    // auth: {
+    //     clientId: '163c60b7-7d2e-441f-b605-db4b3291e6d1',
+    //     authority: 'https://login.microsoftonline.com/77e3b5a4-521b-4caf-a71c-1f4d9d0d549e',
+    //     redirectUri: 'http://localhost:3000'
+    // }
 };
 
 export const loginRequest: PopupRequest = {
