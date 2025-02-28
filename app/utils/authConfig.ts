4170889861494a87f8eca5ff37b550acdf6b6444
AmmarFahmy <ammarkhan375@gmail.com>
2025-02-28 18:21:17 +0530

    Update Azure AD authentication configuration for production deployment

--- app/utils/authConfig.ts
+++ app/utils/authConfig.ts
@@ -2,9 +2,9 @@ import { Configuration, PopupRequest } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: '163c60b7-7d2e-441f-b605-db4b3291e6d1',
+        clientId: 'dd2e0cf6-894b-4816-ba10-01a45d1447c6',
         authority: 'https://login.microsoftonline.com/77e3b5a4-521b-4caf-a71c-1f4d9d0d549e',
-        redirectUri: 'http://localhost:3000'
+        redirectUri: 'https://ms-graph-next-app.vercel.app'
     }
 };
 
